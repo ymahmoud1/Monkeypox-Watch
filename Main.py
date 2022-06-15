0156cec6d689b24cd55743f129c04896fb54a5ff
ymahmoud1 <89483387+ymahmoud1@users.noreply.github.com>
2022-06-15 17:49:53 +0400

    Add files via upload
    Made Two Plotly graphs for some clean yet simple data visualization of the monkeypox virus. 
    Data: Global.health Monkeypox (accessed on 2022-06-15)

---
+++ Main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from plotly import __version__
+import cufflinks as cf
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+init_notebook_mode(connected=True)
+cf.go_offline()
+import plotly.express as px
+
+if __name__ == '__main__':
+    mp_data = pd.read_csv('https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv')
+    conf_cases = mp_data[mp_data['Status'] == 'confirmed']
+    fig1 = px.bar(conf_cases.groupby('Country').count().reset_index(), x='Country', y='ID',
+                  title='Confirmed Cases in Countries (Figure 1)', labels={'ID': 'Total Cases'}, text_auto=True)
+    fig2 = px.line(conf_cases.groupby('Date_confirmation').count().reset_index(),
+                   x='Date_confirmation', y='ID', title='Daily Infections',
+                   labels={'ID': 'Confirmed Cases', 'Date_confirmation': 'Date'}, markers=True)
+    fig1.show()
+    fig2.show()
